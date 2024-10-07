d2e1a768ccc5b5f1f345bd71a958bf1d5d220b49
Zohaib Ahmad <zohaibsattar1039@gmail.com>
2024-10-08 00:19:26 +0500

    cors policy defined for ws connection

--- index.js
+++ index.js
@@ -4,13 +4,25 @@ import { createServer } from "http";
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
 app.get("/", (req, res) => {
   res.send("Hello From Server");
 });
 
 io.on("connection", (socket) => {
-  console.log("A new user make a web socket connection");
+  console.log("A new user make a web socket connection", socket.id);
+
+  socket.on("disconnect", (reason) => {
+    console.log(
+      `User with socket id ${socket.id} disconnected. Reason: ${reason}`
+    );
+  });
 });
 server.listen(3000, () => {
   console.log("Server is listening at port 3000");
